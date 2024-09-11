57a7deb4cee8f1637a499da818037969d8ad2250
Hecze <hectorzerrillo85@gmail.com>
2024-09-11 11:30:33 -0500

    bordes redondeados, iconos agregados y padding modificado

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -26,52 +26,52 @@ export default function LoginScreen({ navigation }: any) {
   };
 
   return (
-    <View style={tailwind`flex-1  items-center bg-[#2C36FF] px-5`}>
+    <View style={tailwind`flex-1  items-center bg-[#2C36FF] px-10`}>
 
       <Image
         source={require('../../assets/images/logo1.png')} // Ruta de la imagen
-        style={tailwind`w-28 h-28 my-20`} // Ajusta el tamaño de la imagen usando Tailwind
+        style={tailwind`w-28 mt-12 mb-16`} // Ajusta el tamaño de la imagen usando Tailwind
         resizeMode="contain" // Ajusta el modo de redimensionamiento
       />
       <Text style={[tailwind`text-white text-4xl mb-10`, { fontFamily: 'Tomorrow_700Bold' }]}>
         Iniciar Sesión
       </Text>
-      <View  style={tailwind`flex-row items-center bg-white w-full p-3 rounded mb-3 `}>   
-      <Entypo name="mail" size={24} color="black" style={tailwind`mr-3`} />
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            style={[tailwind`flex-1`, { fontFamily: 'Tomorrow_400Regular' }]}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            placeholder="Email"
-          />
-        )}
-        name="email"
-        rules={{ required: true }}
-      /></View>
-      <View style={tailwind`flex-row items-center bg-white w-full p-3 rounded mb-3`}>   
-      <Ionicons name="key" size={24} color="black" style={tailwind`mr-3`} />
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            style={[tailwind`flex-1`, { fontFamily: 'Tomorrow_400Regular' }]}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            placeholder="Contraseña"
-            secureTextEntry
-          />
-        )}
-        name="password"
-        rules={{ required: true }}
-      /></View>
+      <View style={tailwind`flex-row items-center bg-white w-full p-3 rounded-xl mb-6 shadow-lg `}>
+        <Entypo name="mail" size={24} color="black" style={tailwind`mr-3 opacity-80`} />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[tailwind`flex-1 opacity-60`, { fontFamily: 'Tomorrow_400Regular' }]}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Email"
+            />
+          )}
+          name="email"
+          rules={{ required: true }}
+        /></View>
+      <View style={tailwind`flex-row items-center bg-white w-full p-3 rounded-xl mb-6 shadow-lg`}>
+        <Ionicons name="key" size={24} color="black" style={tailwind`mr-3 opacity-80`} />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[tailwind`flex-1 opacity-60`, { fontFamily: 'Tomorrow_400Regular' }]}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Contraseña"
+              secureTextEntry
+            />
+          )}
+          name="password"
+          rules={{ required: true }}
+        /></View>
       <TouchableOpacity
         onPress={handleSubmit(onSubmit)}
-        style={tailwind`bg-blue-700 p-4 rounded w-full items-center mb-3`}
+        style={tailwind`bg-blue-700 p-4 rounded w-full items-center mb-3 mt-12 rounded-xl shadow-xl`}
       >
         <Text style={[tailwind`text-white font-bold`, { fontFamily: 'Tomorrow_700Bold' }]}>
           Ingresar
