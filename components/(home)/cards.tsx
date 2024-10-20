c418d532c72d47a620ef621edf724a4b77ea195a
jordan <jordanelcracken@gmail.com>
2024-10-20 06:40:03 -0500

    feat: add styles

--- components/(home)/cards.tsx
+++ components/(home)/cards.tsx
@@ -5,14 +5,14 @@ import { images } from '@/constants'
 const Cards = ({ label, img }: { label: string, img: any }) => {
   const imagen = `../../assets/images/${img}.png`
   return (
-    <View className='rounded-md my-2 mx-2 bg-white w-[180px] h-[200px] justify-center items-center '>
+    <View className='rounded-md my-2 mx-2 bg-white w-[180px] h-[195px] justify-center items-center '>
       <Image
         source={img}
         className='w-[80px] h-[50px]'
         resizeMode='contain'
         alt='not rendered at all'
       />
-      <Text>{label}</Text>
+      <Text className='text-[15px] font-plight mt-5'>{label}</Text>
     </View>
   )
 }
